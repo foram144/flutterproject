010d876ed0ce1bd7c556c2b4fd41f7ec557057ee
foram144 <99945801+foram144@users.noreply.github.com>
2023-01-19 17:03:52 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_WFnCfunyF', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2q77g80pp85du9k9e3ct3d4gv6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'N.Virgina' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
